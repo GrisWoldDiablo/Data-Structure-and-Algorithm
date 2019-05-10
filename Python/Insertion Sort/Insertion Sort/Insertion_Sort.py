012337e6688933e5146fef381df852c81015c3f1
GrisWoldDiablo <griswoldd@hotmail.com>
2019-05-10 15:08:59 -0400

    Python Insertion Sort and more
    Added Insertion Sort Python version.
    Added a PrintList method

--- Python/Insertion Sort/Insertion Sort/Insertion_Sort.py
+++ Python/Insertion Sort/Insertion Sort/Insertion_Sort.py
@@ -5,18 +5,18 @@ Date: May 2019
 import random
 import time
 
-ALGORITHM_NAME = "Bubble Sort" # The name of the algorithm used for sorting
+ALGORITHM_NAME = "Insertion Sort" # The name of the algorithm used for sorting
 random.seed(2019) # Random Seed number to keep values constant.
 LIST_SIZE = 25000 # Set the size of the list to 25,000.
 
 def main():
     myList = [0] * LIST_SIZE # A list to be sorted.
     PopulateList(myList) # Fill the list with random numbers.
-
+    
     #PrintList(myList) # Display list before sorting.
 
     ticks = time.time() # Get the time before sorting.
-    BubbleSort(myList) # Sort the list
+    InsertionSort(myList) # Sort the list
     ticks = time.time() - ticks # Get the time difference after sorting.
 
     #PrintList(myList) # Display list after sorting.
@@ -25,27 +25,31 @@ def main():
     print("Algorithm: ", ALGORITHM_NAME) # Print the name of the algorithm used.
     print("Total Seconds : ", ticks) # Print the time spent in seconds.
 
-def BubbleSort(A:list):
+def InsertionSort(A:list):
     """
-    Sort the list using bubble sort algorithm.
+    Sort the array using insertion sort algorithm.
     -----PSEUDO CODE-----
     (A is an Array with index 0..n)
-    BubbleSort(A)
-     for i = 0 to length of A - 1
-         for j = i - 1 to length of A - 1
-             if A[j] < A[i]
-                 swap A[j] and A[i]
+    InsertionSort(A)
+     for j = 1 to length of A - 1
+         key = A[j]
+         i = j - 1
+         while i >= 0 and A[i] > key
+             A[i + 1] = A[i]
+             i = i + 1
+         A[i+1] = key
     -----PSEUDO CODE-----
     
     Keyword arguments:
     A:list: the list to be sorted
     """
-    for i in range(0, LIST_SIZE):
-        for j in range(i + 1, LIST_SIZE):
-            if (A[j] < A[i]):
-                temp = A[j]
-                A[j] = A[i]
-                A[i] = temp
+    for j in range(0, LIST_SIZE):
+        key = A[j]
+        i = j - 1
+        while i >= 0 and A[i] > key:
+        	A[i + 1] = A[i]
+        	i -= 1
+        A[i + 1] = key
 
 def PopulateList(A:list):
     """
