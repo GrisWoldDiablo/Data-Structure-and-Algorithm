29b49309b9ee376935e065c86cffde886ead1ecc
GrisWoldDiablo <griswoldd@hotmail.com>
2019-05-16 16:16:55 -0400

    C# Singly Linear Linked List
    Added the Singly Linear Linked List C# version
    Also prepare the abstract class for future linked list types.
    And made a method to test the different types.

---
+++ C Sharp/Linked List/Linked List/Linked List.cs
@@ -0,0 +1,353 @@
+﻿/*
+ * Author: Alexandre Lepage
+ * Date: May 2019
+ */
+using System;
+
+namespace Linked_List
+{
+    /// <summary>
+    /// Custom linked list class,
+    /// Linear and Singly
+    /// </summary>
+    /// <typeparam name="T">can be of any type, needs to implement IComparable</typeparam>
+    class TheSinglyLinearLinkedList<T> : TheLinkedList<T> where T : IComparable
+    {
+        /// <summary>
+        /// Class custructor
+        /// -----PSEUDO CODE-----
+        /// LinkedList()
+        ///  head = NIL
+        ///  size = 0
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        public TheSinglyLinearLinkedList()
+        {
+            head = null;
+            size = 0;
+        }
+
+        /// <summary>
+        /// Add a new value to the list
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, k is the value to be added)
+        /// Insert(L,k)
+        ///  y.key = k
+        ///  Insert(L,y)
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="k">value to be added</param>
+        public override void Insert(T k)
+        {
+            Insert(new TSLNode<T>(k));
+        }
+
+        /// <summary>
+        /// Add a new node to the list
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, x is the node to be added)
+        /// Insert(L,x)
+        ///  x.next = L.head
+        ///  L.head = x
+        ///  L.size = L.size + 1
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="x">node to be added</param>
+        public override void Insert(TheNode<T> x)
+        {
+            x.next = head;
+            head = x;
+            size++;
+        }
+
+        /// <summary>
+        /// remove a node with a specific value from the list
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, k is the value of the node to be deleted)
+        /// Delete(L,k)
+        ///  y.key = k
+        ///  Delete(L,y)
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="k">value of the node to be removed</param>
+        /// <returns>True if node was removed</returns>
+        public override bool Delete(T k)
+        {
+            return Delete(new TSLNode<T>(k));
+        }
+
+        /// <summary>
+        /// remove a node from the list
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, x is the node to be deleted)
+        /// Delete(L,x)
+        ///  y = Predecessor(L,x)
+        ///  if y =/= NIL
+        ///     y.next = y.next.next
+        ///     L.size = L.size - 1
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="x">node to be removed</param>
+        /// <returns>True if node was removed</returns>
+        public override bool Delete(TheNode<T> x)
+        {
+            TheNode<T> y = Predecessor(x);
+            if (y != null)
+            {
+                y.next = y.next.next;
+                size--;
+                return true;
+            }
+            return false;
+        }
+
+
+        /// <summary>
+        /// find the node predecessing the node with a specific value
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, k is the value of the node to look at)
+        /// Predecessor(L,k)
+        ///  y.key = k
+        ///  return Predecessor(L,y)
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="k">value of the node to look at</param>
+        /// <returns>the found node or null</returns>
+        public override TheNode<T> Predecessor(T k)
+        {
+            return Predecessor(new TSLNode<T>(k));
+        }
+
+        /// <summary>
+        ///  find the node predecessing the node
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, x is the value of the node to look at)
+        /// Predecessor(L,x)
+        ///  y = L.head
+        ///  if y == NIL
+        ///     return NIL
+        ///  while y.next =/= NIL
+        ///     if y.next == x
+        ///         return y
+        ///     y = y.next
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="n">node to look at</param>
+        /// <returns>the found node or null</returns>
+        public override TheNode<T> Predecessor(TheNode<T> x)
+        {
+            TheNode<T> y = head;
+            if (y == null)
+            {
+                return null;
+            }
+            while (y.next != null)
+            {
+                if (y.next.CompareTo(x) == 0)
+                {
+                    return y;
+                }
+                y = y.next;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// find a node with a specific value
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, k is the value to be found)
+        /// Search(L,k)
+        ///  y.key = k
+        ///  return Search(L,y)
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="k">value of the node to find</param>
+        /// <returns>the found node or null</returns>
+        public override TheNode<T> Search(T k)
+        {
+            return Search(new TSLNode<T>(k));
+        }
+
+        /// <summary>
+        /// find a node with a specific value
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, x is the node to find)
+        /// Search(L,x)
+        ///  y = L.head
+        ///  while y =/= NIL and y =/= x
+        ///     y = y.next
+        ///  return y
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="x">value of the node to find</param>
+        /// <returns>the found node or null</returns>
+        public override TheNode<T> Search(TheNode<T> x)
+        {
+            TheNode<T> y = head;
+            while (y != null && y.CompareTo(x) != 0)
+            {
+                y = y.next;
+            }
+            return y;
+        }
+        /// <summary>
+        /// check if the list contains a node with a specific value
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, k is the value to look for)
+        /// Contains(L,k)
+        ///  y.key = k
+        ///  return Contains(L,y)
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="n">the node to find</param>
+        /// <returns>True if the list contain the node</returns>
+        public override bool Contains(T k)
+        {
+            return Contains(new TSLNode<T>(k));
+        }
+
+        /// <summary>
+        /// check if the list contains a node
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList, x is the node to look for)
+        /// Contains(L,x)
+        ///  y = L.head
+        ///  while y =/= NIL
+        ///     if y == x
+        ///         return True
+        ///     y = y.next
+        ///  return False
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="n">the node to find</param>
+        /// <returns>True if the list contain the node</returns>
+        public override bool Contains(TheNode<T> x)
+        {
+            TheNode<T> y = head;
+            while (y != null)
+            {
+                if (y.CompareTo(x) == 0)
+                {
+                    return true;
+                }
+                y = y.next;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Empty the list
+        /// -----PSEUDO CODE-----
+        /// (L is the LinkedList)
+        /// Clear(L)
+        ///  L.head = NIL
+        ///  L.size = 0
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        public override void Clear()
+        {
+            head = null;
+            size = 0;
+        }
+    } // End Class TheSinglyLinearLinkedList
+
+    /// <summary>
+    /// Custom node class for Singly Linear Linked List
+    /// </summary>
+    /// <typeparam name="T">can be of any type, needs to implement IComparable</typeparam>
+    public class TSLNode<T> : TheNode<T> where T : IComparable
+    {
+        /// <summary>
+        /// Class constructor
+        /// -----PSEUDO CODE-----
+        /// (x is the node, k is the value of the node)
+        /// TSLNode(x,k)
+        ///  x.key = k
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        /// <param name="k">value of the node</param>
+        public TSLNode(T k)
+        {
+            this.key = k;
+        }
+
+    } // End Class TSLNode
+
+    public abstract class TheLinkedList<T> where T : IComparable
+    {
+        protected TheNode<T> head; // node at the head of the list
+        protected int size; // quantity of node(s) are in the list
+
+        public int Count { get => size; } // Return the size of the list
+
+        public abstract void Insert(T k);
+        public abstract void Insert(TheNode<T> n);
+        public abstract bool Delete(T k);
+        public abstract bool Delete(TheNode<T> n);
+        public abstract TheNode<T> Predecessor(T k);
+        public abstract TheNode<T> Predecessor(TheNode<T> n);
+        public abstract TheNode<T> Search(T k);
+        public abstract TheNode<T> Search(TheNode<T> n);
+        public abstract bool Contains(T k);
+        public abstract bool Contains(TheNode<T> n);
+        public abstract void Clear();
+
+        /// <summary>
+        ///  Display all the element of the list
+        /// -----PSEUDO CODE-----
+        /// (L is the list)
+        /// PrintList(L)
+        ///  y = L.head
+        ///  while y =/= NIL
+        ///     print y.key
+        ///     y = y.next
+        /// -----PSEUDO CODE-----
+        /// </summary>
+        public void PrintList()
+        {
+            Console.WriteLine("---- The list elements ----");
+            TheNode<T> y = head;
+            if (y == null)
+            {
+                Console.Write("EMPTY");
+            }
+            while (y != null)
+            {
+                Console.Write($"{y}, ");
+                y = y.next;
+            }
+            Console.WriteLine("\n---- ----");
+        }
+    } // End Class TheLinkedList
+
+    public abstract class TheNode<T> where T : IComparable
+    {
+        public T key; // value of the node
+        public TheNode<T> next; // node after this one in the list
+        protected TheNode<T> prev; // node before this one in the list
+
+        /// <summary>
+        /// compares this object to the argument one.
+        /// </summary>
+        /// <param name="obj">object to compare to</param>
+        /// <returns>-1(SmallerThan), 0(EqualTo), 1(LargerThan) </returns>
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            if (obj is TheNode<T> otherNode)
+                return key.CompareTo(otherNode.key);
+            else
+                throw new ArgumentException($"Object is not a {typeof(TheNode<T>)}");
+        }
+
+        /// <summary>
+        /// Overrides to return the key
+        /// </summary>
+        /// <returns>key value in string form</returns>
+        public override string ToString()
+        {
+            return key.ToString();
+        }
+    }  // End Class TheNode
+} //End Namespace
---
+++ C Sharp/Linked List/Linked List/Program.cs
@@ -0,0 +1,145 @@
+﻿/*
+ * Author: Alexandre Lepage
+ * Date: May 2019
+ */
+using System;
+
+namespace Linked_List
+{
+    class Program
+    {
+        const string DATA_STRUCTURE_NAME = "Linked Lists"; // The name of the data structure
+        const int RANDOM_SEED = 2019; // Random Seed number to keep values constant.
+        static Random randGen = new Random(RANDOM_SEED); // Get a random generator.
+        const int LIST_SIZE = 1000000; // Set the size of the list to 1,000,000.
+        const int TEST_AMOUNT = 500; // Amount of item that will be used for some testing.
+
+        // ---- Array for testing setup ----
+        static int[] myArray = new int[LIST_SIZE];
+        static int[] delTestArray = new int[TEST_AMOUNT];
+        static int[] searchTestArray = new int[TEST_AMOUNT];
+        static int[] containTestArray = new int[TEST_AMOUNT];
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"---- Data structure : {DATA_STRUCTURE_NAME} ----");
+
+            // ---- Setting up the values for testing ----
+            PopulateArray(myArray);
+            GetRandomValues(myArray, delTestArray);
+            GetRandomValues(myArray, searchTestArray);
+            GetRandomValues(myArray, containTestArray);
+
+            // ---- Classes Speed Test portion ----
+            Console.WriteLine($"---- Testing list speed. ----");
+
+            // ---- Singly Linear Linked List ----
+            Console.WriteLine($"\n---- Class type : Singly Linear Linked List ----");
+            TheSinglyLinearLinkedList<int> mySLLList = new TheSinglyLinearLinkedList<int>();
+            LinkedListTest(mySLLList);
+
+        }
+
+        /// <summary>
+        /// Perform a series of test to a linked list class and calculate its time.
+        /// Test are: Inserting, Deleting values, searching values and deleting found nodes.
+        /// </summary>
+        /// <typeparam name="T">can be of any type, needs to implement IComparable</typeparam>
+        /// <param name="testList">the linked list to be tested</param>
+        private static void LinkedListTest<T>(TheLinkedList<T> testList) where T : IComparable
+        {
+            #region INSERT_TEST
+            // Speed test: Insert LIST_SIZE elements to the list
+            Console.Write($"- Inserting {LIST_SIZE} elements -");
+            long time = DateTime.Now.Ticks; // Get the current ticks.
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                testList.Insert((dynamic)myArray[i]);
+            }
+            time = DateTime.Now.Ticks - time; // Get the time spent inserting.
+            long totalTime = time; // Get the time spend for this test.
+            Console.WriteLine($"- {testList.Count} elements inserted. -");
+            Console.WriteLine($"\tTotal Seconds: {TimeSpan.FromTicks(time).TotalSeconds}"); // Print the time spent in seconds.
+            #endregion
+
+            #region DELETE_VALUE_TEST
+            // Speed test: Delete TEST_AMOUNT elements from the list
+            Console.Write($"- Deleting {TEST_AMOUNT} values. -");
+            time = DateTime.Now.Ticks; // Get the current ticks.
+            for (int i = 0; i < delTestArray.Length; i++)
+            {
+                testList.Delete((dynamic)delTestArray[i]); // Delete using value
+            }
+            time = DateTime.Now.Ticks - time; // Get the time spent Deleting.
+            totalTime += time; // Add the time spend for this test
+            Console.WriteLine($"- {testList.Count} nodes left. -");
+            Console.WriteLine($"\tTotal Seconds: {TimeSpan.FromTicks(time).TotalSeconds}"); // Print the time spent in seconds.
+            #endregion
+
+            #region SEARCH_VALUE_TEST
+            // Speed test: Search TEST_AMOUNT elements from the list
+            TheNode<int>[] theFoundNodes = new TheNode<int>[TEST_AMOUNT]; // Found nodes from this test
+            Console.Write($"- Searching {TEST_AMOUNT} values. -");
+            time = DateTime.Now.Ticks; // Get the current ticks.
+            for (int i = 0; i < searchTestArray.Length; i++)
+            {
+                theFoundNodes[i] = testList.Search((dynamic)searchTestArray[i]); // The found nodes are placed in an array for further testing.
+            }
+            time = DateTime.Now.Ticks - time; // Get the time spent Searching.
+            totalTime += time; // Add the time spend for this test
+            int foundNodes = 0;
+            foreach (var item in theFoundNodes) // Check the found nodes
+            {
+                if (item != null) // Count how many nodes are null meaning not found.
+                {
+                    foundNodes++;
+                }
+            }
+            Console.WriteLine($"- {foundNodes} nodes found. -");
+            Console.WriteLine($"\tTotal Seconds: {TimeSpan.FromTicks(time).TotalSeconds}"); // Print the time spent in seconds.
+            #endregion
+
+            #region DELETE_NODE_TEST
+            // Speed test: Deleting TEST_AMOUNT node from the list
+            Console.Write($"- Deleting {foundNodes} nodes. -");
+            time = DateTime.Now.Ticks; // Get the current ticks.
+            for (int i = 0; i < theFoundNodes.Length; i++)
+            {
+                testList.Delete((dynamic)theFoundNodes[i]); // Delete using nodes.
+            }
+            time = DateTime.Now.Ticks - time; // Get the time spent Searching.
+            totalTime += time; // Add the time spend for this test
+            Console.WriteLine($"- {testList.Count} nodes left  -");
+            Console.WriteLine($"\tTotal Seconds: {TimeSpan.FromTicks(time).TotalSeconds}"); // Print the time spent in seconds.
+            #endregion
+
+            Console.WriteLine("- Class Speed for all tests -");
+            Console.WriteLine($"\tTotal seconds : {TimeSpan.FromTicks(totalTime).TotalSeconds}"); // Print the time spent in seconds for all tests combined 
+        }
+
+        /// <summary>
+        /// Fill array B with random values from array A.
+        /// </summary>
+        /// <param name="A"></param>
+        /// <param name="B"></param>
+        static void GetRandomValues(int[] A, int[] B)
+        {
+            for (int i = 0; i < B.Length; i++)
+            {
+                B[i] = A[randGen.Next(A.Length)];
+            }
+        }
+
+        /// <summary>
+        /// Populate an int array with random numbers between -100,000 to 100,000
+        /// </summary>
+        /// <param name="A">array to populate</param>
+        static void PopulateArray(int[] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = randGen.Next(-100000,100000);
+            }
+        }
+    } // End class
+} // End namespace
